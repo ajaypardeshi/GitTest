0202aa8fd2c2b3ab7b705d1e4172f7e60af63448
ajay p <ajpardeshi007@gmail.com>
2021-05-15 22:04:58 +0530

    initial commit

---
+++ src/com/avp/main.java
@@ -0,0 +1,15 @@
+package com.avp;
+
+public class main {
+	
+	public void add(int a,int b)
+	{
+		System.out.println(a+b);
+	}
+	public static void main(String[] args) {
+		
+		main m =new main();
+		m.add(10, 20);
+	}
+
+}
