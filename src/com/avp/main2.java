073f78a079efb004b23736200efb7879e78fcf97
mayur <mayurbhosale999@gmail.com>
2021-05-15 23:03:41 +0530

    main2 class divide() method modified as divide2() method by Mayur

--- src/com/avp/main2.java
+++ src/com/avp/main2.java
@@ -10,11 +10,12 @@ public class main2 {
 		System.out.println("this is multiplication");
 	}
 	
-	public void divide() {
-		System.out.println("this is division");
+	public void divide2() {
+		System.out.println("this is division 2 by mayur");
 	}
 	
 	public void add() {
 		System.out.println("this is addition");
 	}
+	
 }
