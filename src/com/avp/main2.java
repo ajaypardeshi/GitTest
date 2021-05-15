c781febb36692e492d6b859ffd804fa115eb7111
mayur <mayurbhosale999@gmail.com>
2021-05-15 23:25:34 +0530

    main2 sub() method removed by Mayur

--- src/com/avp/main2.java
+++ src/com/avp/main2.java
@@ -2,10 +2,7 @@ package com.avp;
 
 public class main2 {
 
-	public void sub() {
-		System.out.println("this is substaction");
-	}
-
+	
 	public void multi() {
 		System.out.println("this is multiplication");
 	}
